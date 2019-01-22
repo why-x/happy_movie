6b3d2aad307f8a2fdafca49dc3f5ae19953a0a84
zyw45198565 <863685516@qq.com>
2019-01-22 20:03:41 +0800

    112

---
+++ app/src/main/java/com/why/happy_movie/activity/MainActivity.java
@@ -0,0 +1,40 @@
+package com.why.happy_movie.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.why.happy_movie.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    int i = 3;
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what==1){
+                i--;
+                if(i<=0){
+                    Intent intent = new Intent(MainActivity.this, JumpActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+                handler.sendEmptyMessageDelayed(1,1000);
+            }
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        handler.sendEmptyMessageDelayed(1,1000);
+
+    }
+}
