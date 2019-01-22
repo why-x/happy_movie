764ff1a2c02e44e767035eb250cb30aa1cf53ec8
why-x <1564760869@qq.com>
2019-01-22 17:28:51 +0800

    me

--- app/src/main/java/com/why/happy_movie/MainActivity.java
+++ app/src/main/java/com/why/happy_movie/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "1111111", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "1111111", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "1111111", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hahahahah", Toast.LENGTH_SHORT).show();
     }
 }
