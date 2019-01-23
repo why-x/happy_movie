268fbeed23df07c7e52cebb0b3e36ee61a60d336
zyw45198565 <863685516@qq.com>
2019-01-23 10:36:58 +0800

    121

--- app/src/main/java/com/why/happy_movie/activity/LoginActivity.java
+++ app/src/main/java/com/why/happy_movie/activity/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.why.happy_movie.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.why.happy_movie.R;
 
@@ -11,5 +14,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView zhuce = findViewById(R.id.btn_regirect_tiao);
+        zhuce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
